db936ba8ab0b39b10c679d09325c035e2de44433
AugustusP <agp5191@psu.edu>
2020-09-03 13:59:11 +0000

    formatted output with special chars

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(void) {
   
   float temp_C = atof(readline("Enter temperature in celsius: "));
 
-  printf("%f in Celsius is equivalent to %f Fahrenheit. \n",temp_C,(temp_C * 9/5 +32));
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit. \n",temp_C,(temp_C * 9/5 +32));
 
   return 0;
 }
